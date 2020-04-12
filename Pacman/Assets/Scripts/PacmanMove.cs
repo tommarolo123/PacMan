13755c94d7d55e3c4d157318300f08bf08da1589
tommarolo123 <tommarolo123@yahoo.co.jp>
2020-04-12 20:29:32 +0900

    計画、素材導入、、Scene作成、アニメション作成、動き方を考える

---
+++ Pacman/Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour
+{
+
+    public float speed = 0.35f;//pacman移動スピード
+    private Vector2 dest = Vector2.zero;//次の移動位置
+
+    private void Start()
+    {
+        dest = transform.position;//初期位置　最初動かないように
+
+    }
+
+}
