188aebc5bbe67d74cf6cf9aa78af2d8fe66cbf6e
tommarolo123 <tommarolo123@yahoo.co.jp>
2020-04-13 01:36:49 +0900

    ぴくマンの移動完成

--- Pacman/Assets/Scripts/PacmanMove.cs
+++ Pacman/Assets/Scripts/PacmanMove.cs
@@ -12,5 +12,42 @@ public class PacmanMove : MonoBehaviour
         dest = transform.position;//初期位置　最初動かないように
 
     }
+    private void FixedUpdate()
+    {
+        Vector2 temp = Vector2.MoveTowards(transform.position, dest, speed);//移動
+        GetComponent<Rigidbody2D>().MovePosition(temp);//Rigidbodyから位置を設定
+        if ((Vector2)transform.position == dest)  //移動間隔を固定する　前のdest位置に達成したらつぎのキー押す動作が効く
+        {
 
+            if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && Valid(Vector2.up)) //　　　　　　　　　　　　　　**Input.getAxis
+            {
+                dest = (Vector2)transform.position + Vector2.up;//　　　　Vector2型に変える　　　z座標いらない
+             
+            }
+            if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && Valid(Vector2.left))
+            {
+                dest = (Vector2)transform.position + Vector2.left;
+            }
+            if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && Valid(Vector2.right))
+            {
+
+                dest = (Vector2)transform.position + Vector2.right;
+            }
+            if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))&& Valid(Vector2.down))
+            {
+                dest = (Vector2)transform.position + Vector2.down;
+            }
+        }
+        }
+
+
+    private bool Valid(Vector2 dir) // read me で説明
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
 }
+
+
+
