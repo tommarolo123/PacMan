7b09828b005554650d030631de29659e1f6c5db1
tommarolo123 <tommarolo123@yahoo.co.jp>
2020-04-15 02:25:51 +0900

    4ルート作成して敵がランダムで動く、偽AI（( ´∀｀ )）

--- Pacman/Assets/Scripts/GhostMove.cs
+++ Pacman/Assets/Scripts/GhostMove.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 public class GhostMove : MonoBehaviour
 {
     //AIを使わないで　敵の移動は決まったルートに従って移動する
-    public GameObject wayPointGo;//public Transform[] wayPoints;//移動ルートの点 
+    public GameObject[] wayPointGos;//public Transform[] wayPoints;//移動ルートの点 
     public float speed = 0.2f;
     private List<Vector3> wayPoints = new List<Vector3>();
     private int index = 0;
     private void Start()
     {
-        foreach (Transform t in wayPointGo.transform)
-        {
-            wayPoints.Add(t.position);
-        }
+        LoadAPath(wayPointGos[Random.Range(0, 4)]);
     }
 
     private void FixedUpdate()
@@ -25,13 +22,31 @@ public class GhostMove : MonoBehaviour
         }
         else
         {
-            index = (index + 1) % wayPoints.Count;
+            index++;
+            
+            if(index >= wayPoints.Count)
+            {
+                index = 0;
+                LoadAPath(wayPointGos[Random.Range(0, 4)]);
+            }
         }
         Vector2 dir = (Vector2)wayPoints[index] - (Vector2)transform.position; //移動方向ベクトルを取得してanimatorに渡す
         GetComponent<Animator>().SetFloat("DirX", dir.x);
         GetComponent<Animator>().SetFloat("DirY", dir.y);
     
     }
+    private void LoadAPath(GameObject go)
+
+
+    {
+
+        wayPoints.Clear();
+        foreach (Transform t in wayPointGos[Random.Range(0, 4)].transform)
+        {
+            wayPoints.Add(t.position);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) //敵がpacmanを食う
     {
         if (collision.gameObject.name == "Pacman")
